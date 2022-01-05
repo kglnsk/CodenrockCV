dbeddcb39d4214075a2d249fe4b461f856d4cb93
glinskiy <glisky@wireless.iitp.ru>
2022-01-05 17:16:09 +0300

    large model

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ COPY ./ /app
 WORKDIR /app
 RUN pip3 install -r requirements.txt
 RUN mkdir -p /root/.cache/torch/hub/checkpoints/
-RUN curl https://download.pytorch.org/models/mobilenet_v3_small-047dcff4.pth -o /root/.cache/torch/hub/checkpoints/mobilenet_v3_small-047dcff4.pth
+RUN curl https://download.pytorch.org/models/mobilenet_v3_large-8738ca79.pth -o https://download.pytorch.org/models/mobilenet_v3_large-8738ca79.pth
 CMD ["python3","run.py"]
--- run.py
+++ run.py
@@ -1,3 +1,49 @@
 #темплейт для запуска решения
+
+
+
+import torch
+from torch import nn
+import torchvision
+from torchvision import transforms,models
+from torch.utils.data import Dataset
+from torch.autograd import Variable
+from PIL import Image
+import os
+import pandas as pd
+import time 
+from tqdm import tqdm
+
+
+def predict_image(image):
+    image_tensor = test_transforms(image).float()
+    image_tensor = image_tensor.unsqueeze_(0)
+    input = Variable(image_tensor)
+    input = input.to(device)
+    output = inference_model(image_tensor)
+    index = output.detach().cpu().numpy().argmax()
+    return index
+
 if __name__ == "__main__":
   print("Ok")
+  device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+  inference_model = torch.load("model.pt")
+  inference_model.eval().cpu()
+  test_transforms = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+  submission = pd.DataFrame(columns = ['image_name','class_id'])
+  idxs = []
+  files = os.listdir('data/test') 
+  for filename in files:
+    image = Image.open(os.path.join('./data/test',filename))
+    index = predict_image(image)
+    idxs.append(index)
+  submission['image_name'] = files
+  submission['class_id'] = idxs
+  submission.to_csv('./data/out/submission.csv',index = False,sep = '\t')
+
+
+
+
